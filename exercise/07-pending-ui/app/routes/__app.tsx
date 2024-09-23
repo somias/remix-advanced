d632bef5746dd115d884cfeec58ecaab824fec44
milos.savkovic <milos.savkovic@nites.eu>
2024-09-23 09:29:55 +0200

    Exercise 7: Show spinner with useTransition and spin-delay

--- exercise/07-pending-ui/app/routes/__app.tsx
+++ exercise/07-pending-ui/app/routes/__app.tsx
@@ -1,5 +1,6 @@
-import { Form, Link, NavLink, Outlet } from "@remix-run/react";
+import { Form, Link, NavLink, Outlet, useTransition } from "@remix-run/react";
 import clsx from "clsx";
+import { useSpinDelay } from "spin-delay";
 import {
   FullFakebooksLogo,
   LogoutIcon,
@@ -10,6 +11,10 @@ import {
 export default function AppRoute() {
   // 🐨 add useTransition here.
   // 💯 add useSpinDelay (from 'spin-delay') here
+  const transition = useTransition();
+  const loading = transition.state !== "idle";
+  const showSpinner = useSpinDelay(loading, { delay: 300, minDuration: 200 });
+
   return (
     <div className="relative flex h-full rounded-lg bg-white text-gray-600">
       <div className="border-r border-gray-100 bg-gray-50">
@@ -19,6 +24,7 @@ export default function AppRoute() {
               <FullFakebooksLogo size="sm" position="left" />
             </Link>
             {/* 🐨 add the <Spinner /> here and show it when we're transitioning */}
+            <Spinner visible={showSpinner} />
           </div>
           <div className="h-7" />
           <div className="flex flex-col font-bold text-gray-800">
